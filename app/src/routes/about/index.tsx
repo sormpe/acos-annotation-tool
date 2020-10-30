2b7505c41cb8bed6f3740c98e9c7bf5d077ae227
Peter Sormunen <peter.sormunen@outlook.com>
2020-10-30 15:21:51 +0200

    Write some content to about page.

--- app/src/routes/about/index.tsx
+++ app/src/routes/about/index.tsx
@@ -6,7 +6,17 @@ const About: FunctionalComponent = () => {
   return (
     <div class={style.about}>
       <h2>Code annotion tool</h2>
-      <p>This tool is lorem ipsum...</p>
+      <p>
+        With this tool you can easily create and preview annotations. It genererates both reStructuredText and JSON notations for later usage, for
+        example creating ACOS content packages.
+      </p>
+      <p>Currently, it supports plain text and following programming and markup languages:</p>
+      <ul>
+        <li>JavaScript</li>
+        <li>Python</li>
+        <li>CSS</li>
+        <li>C</li>
+      </ul>
     </div>
   );
 };
