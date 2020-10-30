034799e002ab83939658f3d6c108f4a296c89372
Peter Sormunen <peter.sormunen@outlook.com>
2020-10-30 15:20:01 +0200

    Remove unused hook.

--- app/src/hooks/useForceUpdate.ts
+++
@@ -1,8 +0,0 @@
-import { useState } from 'preact/hooks';
-
-//create your forceUpdate hook
-export function useForceUpdate() {
-  const [value, setValue] = useState(0); // integer state
-  console.log('FORCE UPDATE');
-  return () => setValue((value) => ++value); // update the state to force render
-}
