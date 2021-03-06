433dec3d682cb0cc1730450cb04c01a7833c6d41
Peter Sormunen <peter.sormunen@outlook.com>
2021-03-06 20:36:16 +0200

    Config.

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    browser: true,
+    browser: true
   },
   plugins: ['@typescript-eslint'],
   extends: [
@@ -12,30 +12,30 @@ module.exports = {
     'plugin:react-hooks/recommended',
     'plugin:prettier/recommended',
     'prettier/@typescript-eslint',
-    'prettier/react',
+    'prettier/react'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
-    project: './tsconfig.eslint.json',
+    project: './tsconfig.eslint.json'
   },
   rules: {
-    'react/no-unknown-property': ['error', { ignore: ['class'] }],
+    'react/no-unknown-property': ['error', { ignore: ['class'] }]
   },
   settings: {
     react: {
       pragma: 'h',
-      version: 'detect',
-    },
+      version: 'detect'
+    }
   },
   overrides: [
     {
       files: ['*.js'],
       rules: {
-        '@typescript-eslint/explicit-function-return-type': 'off',
-      },
-    },
-  ],
+        '@typescript-eslint/explicit-function-return-type': 'off'
+      }
+    }
+  ]
 };
